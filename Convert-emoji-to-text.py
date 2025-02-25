da567876d87b5bb44ba273502bbaf91b421e1e04
Muskan Saleh <muskaankorejo66@gmail.com>
2025-02-25 20:41:17 +0500

    Convert-emoji-to-text.py

--- Convert-emoji-to-text.py
+++ Convert-emoji-to-text.py
@@ -1,5 +1,8 @@
 import demoji
  
 demoji.download_codes()
-text = "I will bw your secret santa this year 🤶🏼 "
+text = """I will bw your secret santa this year 🤶🏼.
+          The party I am hostig for you is going to be lit.
+
+                               """
 demoji.findall(text)
